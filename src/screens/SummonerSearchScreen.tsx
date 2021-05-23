7e96ed36f2dce29180e9f5ae67ddb6b9388a3860
Julian-Ewald <julianewald0707@gmail.com>
2021-05-24 01:08:27 +0200

    update match history kda...

--- src/screens/SummonerSearchScreen.tsx
+++ src/screens/SummonerSearchScreen.tsx
@@ -2,17 +2,19 @@ import React, { useEffect, useState, useRef } from 'react';
 import { Formik } from 'formik';
 import { IoSearch } from "react-icons/io5"
 import '../App.global.css';
+import { loadPartialConfig } from '@babel/core';
 
 export default function SummonerSearchScreen () {
 
     const scrollToBtm = useRef<any>(null)
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const [modalVisible, setModalVisible] = useState(false);
     const [status, setStatus] = useState<any>(-99);
     const [summoner, setSummoner] = useState<any>();
 
     const [matches, setMatches] = useState<any>([]);
-    const [playerPos, setPlayerPos] = useState<any>(0);
+    const [playerPos, setPlayerPos] = useState<any>([]);
 
     return (
     <div className="bigContainer" style={{backgroundColor: '#1D1D1D' }}>
@@ -20,6 +22,7 @@ export default function SummonerSearchScreen () {
             initialValues={{ summonerName: '' }}
             onSubmit={async (values, { setSubmitting }) => {
                 setMatches([])
+                setIsLoading(true)
                 const response = await fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${values.summonerName}`, {
                     headers: {
                         "X-Riot-Token": "RGAPI-7a684882-6c53-4714-9914-ff9803f332f1"
@@ -64,6 +67,7 @@ export default function SummonerSearchScreen () {
                     temp = []
                     temp1 = []
                 })
+                setIsLoading(false)
                 scrollToBtm.current?.scrollIntoView({behavior: 'smooth'})
                 setSubmitting(false)
             }}
@@ -89,79 +93,112 @@ export default function SummonerSearchScreen () {
                 </div>
             )
         }
-        <div style={{width: "100%", height: "100%"}}>
-            <div style={{padding: 10}}>
-                {status == 200 ? (
-                    <div style={{display: 'flex', flexDirection: 'row'}}>
-                        <div>
-                            <img 
-                                style={{width: 100, height: "auto", borderRadius: 50, borderStyle: 'solid', borderWidth: 2, borderColor: '#C4C4C4'}} 
-                                src={`http://ddragon.leagueoflegends.com/cdn/11.10.1/img/profileicon/${summoner.profileIconId}.png`} 
-                            />
-                            <div className="summonerLvl" style={{
-                                backgroundColor: '#171717',
-                                padding: 5,
-                                width: 50,
-                                borderRadius: 10,
-                                color: '#C4C4C4',
-                                borderStyle: 'solid',
-                                borderWidth: 2,
-                                borderColor: '#C4C4C4',
-                                textAlign: 'center'
-                            }}>
-                                {summoner.summonerLevel}
-                            </div>
-                            <div className="championText" style={{
-                                color: '#C4C4C4', 
-                                letterSpacing: 0.5, 
-                                fontSize: 18, 
-                                whiteSpace: 'nowrap'
-                            }}>
-                                {summoner.name}
-                            </div>
-                        </div>
-                        <div className="matchH" style={{
-                            width: "100%", 
-                            backgroundColor: "#404040",
-                            borderRadius: 10,
-                            marginLeft: 20,
-                            padding: 10
-                        }}>
-                            <div style={{
-                                color: '#C4C4C4', 
-                                letterSpacing: 0.5, 
-                                fontSize: 18, 
-                                whiteSpace: 'nowrap',
-                                marginBottom: 15,
-                            }}>
-                                Match History
-                            </div>
-                            <div>
-                                {
-                                    matches.map((x:any, i:number) => 
-                                        <div className="matchHItem" key={x.info.gameId}>
-                                            {
-                                                <div>
-                                                    <div>{x.info.participants[playerPos[i]].championName}</div>
-                                                </div>
-                                            }
-                                        </div>
-                                    )
-                                }
-                            </div>
-                        </div>
-                    </div>
-                ) : status == -99 ? (
-                    <div style={{color: 'whitesmoke'}}>
-                        Search for a User :)
-                    </div>
-                ) : (
-                    <div style={{color: 'whitesmoke'}}>
-                        Invalid Username :(
-                    </div>
-                )}
+        {isLoading ? (
+            <div>
+                Loading...
             </div>
-        </div>
+        ) : (
+            <div style={{width: "100%", height: "100%"}}>
+                <div style={{padding: 10}}>
+                    {status == 200 ? (
+                        <div style={{display: 'flex', flexDirection: 'row'}}>
+                            <div>
+                                <img 
+                                    style={{width: 100, height: "auto", borderRadius: 50, borderStyle: 'solid', borderWidth: 2, borderColor: '#C4C4C4'}} 
+                                    src={`http://ddragon.leagueoflegends.com/cdn/11.10.1/img/profileicon/${summoner.profileIconId}.png`} 
+                                />
+                                <div className="summonerLvl" style={{
+                                    backgroundColor: '#171717',
+                                    padding: 5,
+                                    width: 50,
+                                    borderRadius: 10,
+                                    color: '#C4C4C4',
+                                    borderStyle: 'solid',
+                                    borderWidth: 2,
+                                    borderColor: '#C4C4C4',
+                                    textAlign: 'center'
+                                }}>
+                                    {summoner.summonerLevel}
+                                </div>
+                                <div className="championText" style={{
+                                    color: '#C4C4C4', 
+                                    letterSpacing: 0.5, 
+                                    fontSize: 18, 
+                                    whiteSpace: 'nowrap'
+                                }}>
+                                    {summoner.name}
+                                </div>
+                            </div>
+                            <div className="matchH" style={{
+                                width: "100%", 
+                                borderRadius: 10,
+                                marginLeft: 20,
+                                padding: 10
+                            }}>
+                                <div style={{
+                                    color: '#C4C4C4', 
+                                    letterSpacing: 0.5, 
+                                    fontSize: 18, 
+                                    whiteSpace: 'nowrap',
+                                    marginBottom: 15,
+                                }}>
+                                    Match History
+                                </div>
+                                <div>
+                                    {
+                                        matches.map((x:any, i:number) => 
+                                            <div className="matchHItem" key={x.info.gameId}>
+                                                {
+                                                    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                                                        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                                                            <img style={{width: 40, height: 40, borderRadius: 20, marginRight: 15}} src={`http://ddragon.leagueoflegends.com/cdn/11.10.1/img/champion/${x.info.participants[playerPos[i]].championName}.png`} />
+                                                            <div style={{width: 100}}>{x.info.participants[playerPos[i]].championName}</div>
+                                                        </div>
+                                                        <div style={{textAlign: 'center', alignItems: 'center'}}>
+                                                            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                                                                <div style={{ width: 25, textAlign: "center"}}>{x.info.participants[playerPos[i]].kills}</div>
+                                                                <div>/</div>
+                                                                <div style={{color: "#B94646", width: 25, textAlign: "center"}}>{x.info.participants[playerPos[i]].deaths}</div>
+                                                                <div>/</div>
+                                                                <div style={{ width: 25, textAlign: "center"}}>{x.info.participants[playerPos[i]].assists}</div>
+                                                            </div>
+                                                            <div style={{display: 'flex', flexDirection: 'row', textAlign: "center", justifyContent: 'center', marginTop: 5, fontSize: 12}}>
+                                                                <div style={{marginRight: 5, color: '#6E6E6E'}}>
+                                                                    KDA:
+                                                                </div>
+                                                                <div style={{
+                                                                    color: 
+                                                                        parseFloat(((x.info.participants[playerPos[i]].kills + x.info.participants[playerPos[i]].assists) / 
+                                                                        x.info.participants[playerPos[i]].deaths).toFixed(2)) >= 7.00 ? "#4BB543" : 
+                                                                        parseFloat(((x.info.participants[playerPos[i]].kills + x.info.participants[playerPos[i]].assists) / 
+                                                                        x.info.participants[playerPos[i]].deaths).toFixed(2)) <= 7.00 && 
+                                                                        parseFloat(((x.info.participants[playerPos[i]].kills + x.info.participants[playerPos[i]].assists) / 
+                                                                        x.info.participants[playerPos[i]].deaths).toFixed(2)) >= 3.00 ? '#DC7612' : "#B94646"
+                                                                }}>
+                                                                    {((x.info.participants[playerPos[i]].kills + x.info.participants[playerPos[i]].assists) / x.info.participants[playerPos[i]].deaths).toFixed(2)}
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                }
+                                            </div>
+                                        )
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    ) : status == -99 ? (
+                        <div style={{color: 'whitesmoke'}}>
+                            Search for a User :)
+                        </div>
+                    ) : (
+                        <div style={{color: 'whitesmoke'}}>
+                            Invalid Username :(
+                        </div>
+                    )}
+                </div>
+            </div>
+        )}
         <div ref={scrollToBtm} style={{height: 0}} />
     </div>
     );
